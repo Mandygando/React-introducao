3bc186a1f67787892b0597fd5d189db1d19a8313
Amanda Viória <amandavic7576@gmail.com>
2023-04-13 20:52:00 -0300

    adicionando_nome_foto

--- pages/deputados/hooks.js
+++ pages/deputados/hooks.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import Pagina from '../../components/Pagina'
-import apiDeputados from '../../services/apiDeputados'
-const hooks = () => {
+import React, { useEffect, useState } from 'react';
+import Pagina from '../../components/Pagina';
+import apiDeputados from '../../services/apiDeputados';
 
-  const [deputados, setDeputados] = useState([])
+const Hooks = () => {
+
+  const [deputados, setDeputados] = useState([]);
 
   useEffect(()=>{
 
     apiDeputados.get('/deputados').then(resultado=>{
       setDeputados(resultado.data.dados)
-
+      
     })
   }, [])
 
   return (
     <Pagina titulo="Deputados">
-    {deputados.map(item=>(
-      <img src={item.urlFoto} />
-
-    ))}
+      {deputados.map(item=>(
+        <img title={item.nome} key={item.id} src={item.urlFoto} width="150px" />
+      ))}
     </Pagina>
-    
-  )
+  );
 }
 
-export default hooks
+export default Hooks;
